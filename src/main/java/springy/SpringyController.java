03eb2c13d83e60403ac7fef02e57df1242d5036f
Raphael Weiner <rweiner@pivotallabs.com>
2014-01-07 10:29:23 -0700

    add greeting endpoint

--- src/main/java/springy/SpringyController.java
+++ src/main/java/springy/SpringyController.java
@@ -1,14 +1,23 @@
 package springy;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class SpringyController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
